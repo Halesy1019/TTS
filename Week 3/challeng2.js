fb0576b42cf25ad9118aed3aefa27528f64b76b9
Haley Brightwell <hbrightwell1101@gmail.com>
2022-11-02 08:04:41 -0500

    create java

---
+++ Week 3/challeng2.js
@@ -0,0 +1,7 @@
+let car = >500 miles;
+
+if (car > 500 miles) {
+    console.log("Yes, I think I need an oil change.");
+} else {
+    console.log("My car doesn't need an oil change.")
+}
--- Week 3/challenge1.js
+++ Week 3/challenge1.js
@@ -1,6 +1,6 @@
 let cups = 3;
 
-if (cups > 3) {
+if (temp > 3) {
     console.log("I think I'm ok for now");
 } else {
     console.log("Yes, I'll take another cup of coffee.");
