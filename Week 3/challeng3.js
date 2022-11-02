bbf463021957e66e2533f865b17f1d9894fd36cd
Haley Brightwell <hbrightwell1101@gmail.com>
2022-11-02 08:36:51 -0500

    Added java challenge loop

---
+++ Week 3/challeng3.js
@@ -0,0 +1,8 @@
+for (let bottle = 99; bottle >= 0; bottle --){
+
+    if (bottle === 0) {
+      console.log("Hey! Go buy more root beer!");
+    } else {
+        console.log(bottle + " bottles of root beer on the wall");
+    }
+  }
