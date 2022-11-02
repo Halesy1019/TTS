18dad34b0cf8b45f48b260f5dd529e3b66a19be1
Haley Brightwell <hbrightwell1101@gmail.com>
2022-11-02 08:23:22 -0500

    Add created java challenge 2

--- Week 3/challeng2.js
+++ Week 3/challeng2.js
@@ -1,6 +1,6 @@
-let car = >500 miles;
+let car = 500 ;
 
-if (car > 500 miles) {
+if (car > 500 ) {
     console.log("Yes, I think I need an oil change.");
 } else {
     console.log("My car doesn't need an oil change.")
