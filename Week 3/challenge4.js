d3e28f375e2cb42c05bb8a09c8f903f39d5b174b
Haley Brightwell <hbrightwell1101@gmail.com>
2022-11-03 09:22:26 -0500

    Add all examples if function in java

---
+++ Week 3/challenge4.js
@@ -0,0 +1,17 @@
+// Part 1 example//
+//define variable//
+let billAmount = 75;
+function gratuity(billAmountArg) {
+    let withGratuity= billAmountArg * .2;
+    return withGratuity;
+
+}
+function totalWithGratuity(billAmountArg);
+{ let newTotal= gratuity(billAmountArg) + (billAmountArg);
+    return NewTotal;
+
+}
+console.log("Your total including gratuity is: " + totalWithGratuity(billAmount);
+
+
+
