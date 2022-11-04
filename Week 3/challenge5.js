2cc3efd10adcb68cb507162876719fc8be5db073
Haley Brightwell <hbrightwell1101@gmail.com>
2022-11-03 22:44:58 -0500

    Add definition of javascript attempt

---
+++ Week 3/challenge5.js
@@ -0,0 +1,8 @@
+//challeng 5 day 2 lecture//
+
+//define an array//
+let hands = ['rock', 'paper', 'scissors'];
+function getComputerHand() {
+    //wants you to return hands[0], hands[1], or hands[2]//
+    let myNumberSelection = (Math.random()*10)(% 3)
+}
