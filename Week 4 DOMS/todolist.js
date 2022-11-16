abc3a394474d72f5c520625a7062bdf982a0183d
Haley Brightwell <hbrightwell1101@gmail.com>
2022-11-15 21:20:09 -0600

    Added code to html and js

--- Week 4 DOMS/todolist.js
+++ Week 4 DOMS/todolist.js
@@ -1,1 +1,12 @@
-/* Style the "Submit" button */
+/*Create function for the submit button
+The text from the input box is used to add a list item to the bottom of the list 
+The text from the input box is cleared out.
+When the user clicks on a list item, it is removed
+
+Extra Credit: - When a list item is clicked, cross it out, then remove it after 1 second*/
+
+function onClick(input) {
+    print(input); 
+    return input;
+
+}
